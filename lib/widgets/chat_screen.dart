875728b37b4125ac6119979f2c5d356a26c8fef4
quanlong <wangquanlong@joyme.sg>
2023-05-24 14:09:56 +0800

    chore: auto scroll to bottom of chat window

--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -4,6 +4,7 @@ import 'package:chatgpt/states/chat_ui_state.dart';
 import 'package:chatgpt/states/message_state.dart';
 import 'package:chatgpt/widgets/message_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ChatScreen extends HookConsumerWidget {
@@ -151,8 +152,18 @@ class ChatMessageList extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final messages = ref.watch(messageProvider);
+    final listController = useScrollController();
+
+    ref.listen(messageProvider, (previous, next) {
+      Future.delayed(const Duration(milliseconds: 50), () {
+        listController.jumpTo(
+          listController.position.maxScrollExtent,
+        );
+      });
+    });
 
     return ListView.separated(
+      controller: listController,
       itemBuilder: (context, index) {
         return MessageItem(message: messages[index]);
       },
